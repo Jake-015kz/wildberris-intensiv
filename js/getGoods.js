dd680e01602bf0f6638de620c11c83b21438ecc1
Jake <zhegan89@gmail.com>
2025-10-09 16:26:47 +0500

    Modifying the getGoods.js file

--- js/getGoods.js
+++ js/getGoods.js
@@ -1,36 +1,21 @@
-const getGoods = async () => {
+const getGoods = () => {
   const links = document.querySelectorAll(".navigation-link");
-  const getData = () => {
-    fetch("/db/db.json")
-      .then((response) => response.json())
-      .then((data) => console.log(data));
-  };
-
-  links.forEach((link) => {
-    link.addEventListener("click", () => {
-      getData();
+  const getData = async () => {
+    let data = JSON.parse(localStorage.getItem("data"));
+    if (!data) {
+      const response = await fetch("/db/db.json");
+      data = await response.json();
+      localStorage.setItem("data", JSON.stringify(data));
+    }
+    console.log(data);
+    links.forEach((link) => {
+      link.addEventListener("click", (e) => {
+        e.preventDefault();
+        getData();
+      });
     });
-  });
-
-  localStorage.setItem("data", JSON.stringify([1, 2, 3, 4, 5]));
-  const data = JSON.parse(localStorage.getItem("data"));
-  console.log(data);
+  };
+  getData();
 };
+
 getGoods();
-
-// const getGoods = async () => {
-//   const links = document.querySelectorAll(".navigation-link");
-//   const response = await fetch("db/db.json");
-//   const data = await response.json();
-
-//   links.forEach((link) => {
-//     link.addEventListener("click", () => {
-//       const field = link.dataset.field;
-//       const filteredData = data.filter(
-//         (item) => item[field] === link.textContent
-//       );
-//       console.log(filteredData);
-//     });
-//   });
-// };
-// getGoods();
